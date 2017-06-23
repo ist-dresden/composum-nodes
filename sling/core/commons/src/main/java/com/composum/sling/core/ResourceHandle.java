677152982979aa7c63eb8d726e498d8303425425
Ralf Wunsch <r.wunsch@ist-software.com>
2017-06-23 08:48:20 +0200

    Core: property inheritance extended; code cleanup

--- sling/core/commons/src/main/java/com/composum/sling/core/ResourceHandle.java
+++ sling/core/commons/src/main/java/com/composum/sling/core/ResourceHandle.java
@@ -36,9 +36,8 @@ public class ResourceHandle extends ResourceWrapper {
 
     /** the 'adaptTo' like wrapping helper */
     public static ResourceHandle use(Resource resource) {
-        ResourceHandle handle = resource instanceof ResourceHandle
+        return resource instanceof ResourceHandle
                 ? ((ResourceHandle) resource) : new ResourceHandle(resource);
-        return handle;
     }
 
     /** the universal validation test */
@@ -63,7 +62,7 @@ public class ResourceHandle extends ResourceWrapper {
 
     private transient ResourceHandle contentResource;
     private transient InheritedValues inheritedValues;
-    protected boolean useNodeInheritance = false;
+    protected InheritedValues.Type inheritanceType = InheritedValues.Type.contentRelated;
 
     /**
      * creates a new wrapper instance.
@@ -96,8 +95,7 @@ public class ResourceHandle extends ResourceWrapper {
     }
 
     public <T> T getProperty(String key, Class<T> type) {
-        T value = properties.get(key, type);
-        return value;
+        return properties.get(key, type);
     }
 
     public String getProperty(String key) {
@@ -182,13 +180,17 @@ public class ResourceHandle extends ResourceWrapper {
     // inherited property values
 
     public void setUseNodeInheritance(boolean nodeInheritance) {
-        useNodeInheritance = nodeInheritance;
+        setInheritanceType(InheritedValues.Type.sameContent);
+    }
+
+    public void setInheritanceType(InheritedValues.Type type) {
+        inheritanceType = type;
         inheritedValues = null;
     }
 
     public InheritedValues getInheritedValues() {
         if (inheritedValues == null) {
-            inheritedValues = new InheritedValues(this, useNodeInheritance);
+            inheritedValues = new InheritedValues(this, inheritanceType);
         }
         return inheritedValues;
     }
@@ -200,7 +202,7 @@ public class ResourceHandle extends ResourceWrapper {
     }
 
     public <T> T getInherited(String key, Class<T> type) {
-        T value = (T) getProperty(key, type);
+        T value = getProperty(key, type);
         if (value == null) {
             value = getInheritedValues().get(key, type);
         }
@@ -238,7 +240,7 @@ public class ResourceHandle extends ResourceWrapper {
             }
         }
         if (result == null) {
-            result = getProperties().get (JcrConstants.JCR_PRIMARYTYPE, (String) null);
+            result = getProperties().get(JcrConstants.JCR_PRIMARYTYPE, (String) null);
         }
         return result;
     }
@@ -251,7 +253,7 @@ public class ResourceHandle extends ResourceWrapper {
         if (node != null) {
             try {
                 return node.isNodeType(type);
-            } catch (RepositoryException e) {
+            } catch (RepositoryException ignored) {
             }
         }
         return isResourceType(type);
@@ -359,15 +361,9 @@ public class ResourceHandle extends ResourceWrapper {
         return parent;
     }
 
-
-    /**
-     * @return
-     */
     public String getParentPath() {
-        final String parentPath = ResourceUtil.getParent(getPath());
-        return parentPath;
+        return ResourceUtil.getParent(getPath());
     }
-
 
     /**
      * Retrieves a child of this resource or a parent specified by its base path, name pattern and type;
@@ -451,10 +447,7 @@ public class ResourceHandle extends ResourceWrapper {
      */
     public boolean isRenderable() {
         String resourceType = getResourceType();
-        if (StringUtils.isBlank(resourceType)) {
-            return isRenderableFile();
-        }
-        return true;
+        return !StringUtils.isBlank(resourceType) || isRenderableFile();
     }
 
     /**
