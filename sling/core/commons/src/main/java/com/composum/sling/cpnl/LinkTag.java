cd0b6b606d80beda521e39f9f649b334d69fa548
Ralf Wunsch <r.wunsch@ist-software.com>
2017-11-14 18:21:30 +0100

    cpn:link - copied code reduced

--- sling/core/commons/src/main/java/com/composum/sling/cpnl/LinkTag.java
+++ sling/core/commons/src/main/java/com/composum/sling/cpnl/LinkTag.java
@@ -1,16 +1,9 @@
 package com.composum.sling.cpnl;
 
-import org.apache.commons.lang3.StringUtils;
-
-import javax.servlet.jsp.JspWriter;
-import java.io.IOException;
-
 /**
  * a tag to build hypertext links with mapped URLs
  */
 public class LinkTag extends UrlTag {
-
-    private String alt;
 
     protected String getDefaultTagName() {
         return "a";
@@ -22,23 +15,5 @@ public class LinkTag extends UrlTag {
 
     public void setHref(String href) {
         setUrl(href);
-    }
-
-    protected void clear() {
-        super.clear();
-        alt = null;
-    }
-
-    public void setAlt(String alt) {
-        this.alt = alt;
-    }
-
-    protected void writeAttributes (JspWriter writer) throws IOException {
-        super.writeAttributes(writer);
-        if (StringUtils.isNotBlank(alt)) {
-            writer.write(" alt=\"");
-            writer.write(alt);
-            writer.write("\"");
-        }
     }
 }
