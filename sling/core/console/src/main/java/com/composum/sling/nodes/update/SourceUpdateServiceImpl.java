171354460f3de7eb136c9f41e292edbde4320316
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-02-14 21:18:17 +0100

    some preliminary tries sourceupdate

--- sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
+++ sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
@@ -10,18 +10,25 @@ import org.osgi.service.component.annotations.Activate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
+import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 
+import javax.annotation.Nonnull;
+import javax.jcr.ImportUUIDBehavior;
+import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import javax.jcr.Session;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 import java.io.ByteArrayInputStream;
-import java.io.File;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Stack;
 
 @Component(
         label = "Composum Source Update Service",
@@ -40,17 +47,44 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         saxParserFactory.setNamespaceAware(true);
     }
 
+    /**
+     * Make node equivalent to an XML document. General strategy: we update the attributes of all nodes according to the XML document,
+     * creating nonexistent nodes along the way, and make node which nodes were present, and which were changed.
+     * In a second pass, we recurse through the JCR tree again, delete nodes that were not present and update the lastModified
+     * properties of nodes, below which there were changes.
+     */
     @Override
     public void updateFromXml(Resource resource, InputStream inputStream) throws RepositoryException, IOException, ParserConfigurationException, SAXException {
         // FIXME remove logging output and use stream directly.
         String content = IOUtils.toString(inputStream, "UTF-8");
         LOG.info("Trying to update {} to \n{}", resource.getPath(), content);
-        SAXParser saxParser = saxParserFactory.newSAXParser();
-        File file = new File("test.xml");
-        saxParser.parse(new ByteArrayInputStream(content.getBytes("UTF-8")), new UpdateHandler());
+        Node node = resource.adaptTo(Node.class);
+        Session session = node.getSession();
+        ByteArrayOutputStream sout = new ByteArrayOutputStream();
+        session.exportDocumentView(resource.getPath(), sout, false, false);
+        LOG.info("Document View:\n{}", sout.toString("UTF-8"));
+        try {
+            saxParserFactory.newSAXParser().parse(new ByteArrayInputStream(content.getBytes("UTF-8")), new UpdateHandler(node));
+            SAXParser saxParser = saxParserFactory.newSAXParser();
+            LOG.info("Now importing");
+            XMLReader xmlreader = saxParser.getXMLReader();
+            xmlreader.setContentHandler(session.getImportContentHandler(resource.getPath(), ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING));
+            xmlreader.parse(new InputSource(new ByteArrayInputStream(content.getBytes("UTF-8"))));
+            LOG.info("Have changes: {}", session.hasPendingChanges());
+            session.save();
+        } finally {
+            LOG.info("Have changes (2): {}", session.hasPendingChanges());
+            session.refresh(false); // XXX or save.
+        }
     }
 
-    private class UpdateHandler extends DefaultHandler {
+    protected class UpdateHandler extends DefaultHandler {
+
+        protected Stack<Node> nodeStack = new Stack<>();
+
+        public UpdateHandler(@Nonnull Node top) {
+            nodeStack.push(top);
+        }
 
         @Override
         public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
@@ -82,6 +116,12 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         // error handling stuff
 
         @Override
+        public void warning(SAXParseException e) throws SAXException {
+            // better abort on any warnings - we don't expect any.
+            throw e;
+        }
+
+        @Override
         public void error(SAXParseException e) throws SAXException {
             // better abort on any errors - we don't expect any.
             throw e;
@@ -91,6 +131,12 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
             String chars = new String(ch, start, length);
             // LOG.info("ignorableWhitespace: '{}'", chars);
+        }
+
+        @Override
+        public void skippedEntity(String name) throws SAXException {
+            // That's not normal - don't know how to deal with that.
+            throw new SAXException("Skipped entity " + name);
         }
 
         @Override
