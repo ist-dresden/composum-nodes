f2deaa5314bc90748b34d1f2f63a0c40e88860fc
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-02-19 11:48:45 +0100

    rewrite: basic import

--- sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateService.java
+++ sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateService.java
@@ -3,8 +3,11 @@ package com.composum.sling.nodes.update;
 import org.apache.sling.api.resource.Resource;
 import org.xml.sax.SAXException;
 
+import javax.annotation.Nonnull;
 import javax.jcr.RepositoryException;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -13,5 +16,5 @@ public interface SourceUpdateService {
     /**
      * Reads an XML from the input stream and updates the resource so that it is identical to the stream, ignoring / updating metadata.
      */
-    void updateFromXml(Resource resource, InputStream inputStream) throws RepositoryException, IOException, ParserConfigurationException, SAXException;
+    void updateFromXml(@Nonnull Resource resource, @Nonnull InputStream inputStream) throws RepositoryException, IOException, ParserConfigurationException, SAXException, TransformerException;
 }
--- sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
+++ sling/core/console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
@@ -1,37 +1,34 @@
 package com.composum.sling.nodes.update;
 
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
+import com.composum.sling.core.ResourceHandle;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Service;
-import org.apache.sling.api.resource.Resource;
+import org.apache.jackrabbit.JcrConstants;
+import org.apache.sling.api.resource.*;
 import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.xml.sax.Attributes;
-import org.xml.sax.ContentHandler;
-import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
-import org.xml.sax.SAXParseException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.AttributesImpl;
-import org.xml.sax.helpers.DefaultHandler;
 
+import javax.annotation.Nonnull;
 import javax.jcr.ImportUUIDBehavior;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 import javax.jcr.Session;
+import javax.jcr.nodetype.NodeDefinition;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import javax.xml.transform.*;
+import javax.xml.transform.sax.SAXResult;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
+
+import static com.composum.sling.core.util.ResourceUtil.PROP_LAST_MODIFIED;
+import static com.composum.sling.core.util.ResourceUtil.TYPE_LAST_MODIFIED;
 
 @Component(
         label = "Composum Source Update Service",
@@ -43,12 +40,18 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
     private static final Logger LOG = LoggerFactory.getLogger(SourceUpdateServiceImpl.class);
 
     private SAXParserFactory saxParserFactory;
+    private TransformerFactory transformerFactory;
 
     @Activate
     private void activate(final BundleContext bundleContext) {
         saxParserFactory = SAXParserFactory.newInstance();
         saxParserFactory.setNamespaceAware(true);
+        transformerFactory = TransformerFactory.newInstance();
     }
+
+    private static final Collection<String> ignoredMetadataAttributes = new HashSet<>(Arrays.asList("jcr:uuid", "jcr:lastModified",
+            "jcr:lastModifiedBy", "jcr:created", "jcr:createdBy", "jcr:isCheckedOut", "jcr:baseVersion",
+            "jcr:versionHistory", "jcr:predecessors", "jcr:mergeFailed", "jcr:mergeFailed", "jcr:configuration"));
 
     /**
      * Make node equivalent to an XML document. General strategy: we update the attributes of all nodes according to the XML document,
@@ -57,38 +60,28 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
      * properties of nodes, below which there were changes.
      */
     @Override
-    public void updateFromXml(Resource resource, InputStream inputStream) throws RepositoryException, IOException, ParserConfigurationException, SAXException {
-        // FIXME remove logging output and use stream directly.
-        String content = IOUtils.toString(inputStream, "UTF-8");
-        LOG.info("Trying to update {} to \n{}", resource.getPath(), content);
+    public void updateFromXml(@Nonnull Resource resource, @Nonnull InputStream inputStream) throws RepositoryException, IOException, ParserConfigurationException, SAXException, TransformerException {
+        ResourceResolver resolver = resource.getResourceResolver();
         Node node = resource.adaptTo(Node.class);
         Session session = node.getSession();
         try {
-            UpdateHandler handler = new UpdateHandler();
-            UpdateHandler inputTreeHandler = new UpdateHandler();
-            saxParserFactory.newSAXParser().parse(new ByteArrayInputStream(content.getBytes("UTF-8")), inputTreeHandler);
-            NodeInfo importedRoot = inputTreeHandler.getRoot();
+            Resource vartmp = resolver.getResource("/var/tmp");
+            if (null == vartmp)
+                vartmp = resolver.create(resolver.getResource("/var"), "tmp", Collections.<String, Object>singletonMap(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED));
+            Resource tmpdir = resolver.create(vartmp, UUID.randomUUID().toString(), Collections.<String, Object>singletonMap(JcrConstants.JCR_PRIMARYTYPE, JcrConstants.NT_UNSTRUCTURED));
 
-            UpdateHandler existingTreeHandler = new UpdateHandler();
-            session.exportDocumentView(resource.getPath(), existingTreeHandler, false, false);
-            NodeInfo currentRoot = inputTreeHandler.getRoot();
+            InputStream xslt = getClass().getResourceAsStream("/bundled/sourceupdate/removemetadata.xslt");
+            Transformer transformer = transformerFactory.newTransformer(new StreamSource(xslt));
+            Source source = new StreamSource(inputStream);
 
-            LOG.info("Now importing");
-//            XMLReader xmlreader = saxParserFactory.newSAXParser().getXMLReader();
-//            xmlreader.setContentHandler(session.getImportContentHandler(resource.getPath(), ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING));
-//            xmlreader.parse(new InputSource(new ByteArrayInputStream(content.getBytes("UTF-8"))));
-//            LOG.info("Have changes: {}", session.hasPendingChanges());
+            Result result = new SAXResult(session.getImportContentHandler(tmpdir.getPath(), ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING));
+            transformer.transform(source, result);
 
+            Resource newroot = tmpdir.listChildren().next();
 
-            // this creates difference in boolean attributes: now string. OUCH!
-            ByteArrayOutputStream sout = new ByteArrayOutputStream();
-            session.exportDocumentView(resource.getPath(), sout, false, false);
-            LOG.info("Document View:\n{}", sout.toString("UTF-8"));
+            equalize(newroot, resource, session, resolver);
 
-            XMLReader xmlreader = saxParserFactory.newSAXParser().getXMLReader();
-            xmlreader.setContentHandler(session.getImportContentHandler(resource.getParent().getPath(), ImportUUIDBehavior.IMPORT_UUID_COLLISION_REPLACE_EXISTING));
-            xmlreader.parse(new InputSource(new ByteArrayInputStream(sout.toByteArray())));
-            LOG.info("Have changes: {}", session.hasPendingChanges());
+            session.removeItem(newroot.getPath());
             session.save();
         } finally {
             LOG.info("Have changes (2): {}", session.hasPendingChanges());
@@ -96,108 +89,60 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         }
     }
 
-    protected static String attributesToString(Attributes attributes) {
-        StringBuilder buf = new StringBuilder();
-        for (int i = 0; i < attributes.getLength(); ++i) {
-            buf.append(attributes.getQName(i)).append("=").append(attributes.getValue(i)).append(" ");
-        }
-        return buf.toString();
-    }
+    // XXX unclear: what about changed resource types / mixins?
+    // XXX resource ordering...
+    private void equalize(@Nonnull Resource templateresource, @Nonnull Resource resource, Session session, ResourceResolver resolver)
+            throws PersistenceException, RepositoryException {
+        boolean thisNodeChanged = false;
+        ValueMap templatevalues = ResourceUtil.getValueMap(templateresource);
+        ModifiableValueMap newvalues = resource.adaptTo(ModifiableValueMap.class);
+        if (newvalues == null) throw new IllegalArgumentException("Node not modifiable: " + resource.getPath());
 
-    protected static class NodeInfo {
-        protected String name;
-        protected boolean modified;
-        protected boolean alreadyExists;
-        protected AttributesImpl attributes;
-        protected List<NodeInfo> children = new ArrayList<>();
-
-        @Override
-        public String toString() {
-            return name + "{" + attributesToString(attributes) + "}";
-        }
-    }
-
-    protected static class UpdateHandler extends DefaultHandler {
-
-        protected Stack<NodeInfo> nodeStack = new Stack<>();
-
-        public UpdateHandler() {
-            nodeStack.push(new NodeInfo());
-        }
-
-        public NodeInfo getRoot() {
-            List<NodeInfo> topchildren = nodeStack.peek().children;
-            if (topchildren.isEmpty() || topchildren.size() > 1)
-                throw new IllegalStateException("Bug: wrong number of roots: " + topchildren);
-            return topchildren.get(0);
-        }
-
-        @Override
-        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
-            for (int i = 0; i < attributes.getLength(); ++i) { // sanity check of attributes
-                if (!"CDATA".equals(attributes.getType(i))) {
-                    throw new SAXException("Unknown attribute type " + attributes.getType(i) + " of " + attributes.getQName(i) + "=" + attributes.getValue(i));
-                }
-            }
-            NodeInfo child = new NodeInfo();
-            child.name = qName;
-            child.attributes = new AttributesImpl(attributes);
-            nodeStack.peek().children.add(child);
-            nodeStack.push(child);
-            LOG.info("startElement({})", child);
-        }
-
-        @Override
-        public void endElement(String uri, String localName, String qName) throws SAXException {
-            LOG.info("endElement({})", qName);
-            nodeStack.pop();
-        }
-
-        @Override
-        public void startDocument() throws SAXException {
-            LOG.info("startDocument");
-        }
-
-        @Override
-        public void endDocument() throws SAXException {
-            LOG.info("endDocument");
-        }
-
-        // error handling stuff
-
-        @Override
-        public void warning(SAXParseException e) throws SAXException {
-            // better abort on any warnings - we don't expect any.
-            throw e;
-        }
-
-        @Override
-        public void error(SAXParseException e) throws SAXException {
-            // better abort on any errors - we don't expect any.
-            throw e;
-        }
-
-        @Override
-        public void ignorableWhitespace(char[] ch, int start, int length) throws SAXException {
-            String chars = new String(ch, start, length);
-            // LOG.info("ignorableWhitespace: '{}'", chars);
-        }
-
-        @Override
-        public void skippedEntity(String name) throws SAXException {
-            // That's not normal - don't know how to deal with that.
-            throw new SAXException("Skipped entity " + name);
-        }
-
-        @Override
-        public void characters(char[] ch, int start, int length) throws SAXException {
-            String chars = new String(ch, start, length);
-            // LOG.info("characters: '{}'", chars);
-            if (StringUtils.isNotBlank(chars)) {
-                // That's not normal - don't know how to deal with that.
-                throw new SAXException("Unexpected characters: " + chars);
+        for (Map.Entry<String, Object> entry : templatevalues.entrySet()) {
+            if (!ignoredMetadataAttributes.contains(entry.getKey()) &&
+                    ObjectUtils.notEqual(entry.getValue(), newvalues.get(entry.getKey()))) {
+                thisNodeChanged = true;
+                newvalues.put(entry.getKey(), entry.getValue());
             }
         }
 
+        for (String key : newvalues.keySet()) {
+            if (!ignoredMetadataAttributes.contains(key) && !templatevalues.containsKey(key)) {
+                thisNodeChanged = true;
+                newvalues.remove(key);
+            }
+        }
+
+        for (Resource child : resource.getChildren()) { // XXX are there equally named children somewhere?
+            Resource templateChild = templateresource.getChild(child.getName());
+            if (templateChild == null) {
+                thisNodeChanged = true;
+                resource.getResourceResolver().delete(child);
+            } else {
+                equalize(templateChild, child, session, resolver);
+            }
+        }
+
+        for (Resource templateChild : templateresource.getChildren()) {
+            if (null == resource.getChild(templateChild.getName())) {
+                session.move(templateChild.getPath(), resource.getPath() + "/" + templateChild.getName());
+                thisNodeChanged = true;
+            }
+        }
+
+        NodeDefinition definition = resource.adaptTo(Node.class).getDefinition();
+        if (definition.allowsSameNameSiblings()) LOG.warn("Same name siblings in " + resource.getPath());
+        if (definition.getDeclaringNodeType().hasOrderableChildNodes())
+            LOG.warn("Orderable subnodes not supported yet: " + resource.getResourceType() + " at " + resource.getPath());
+
+        if (thisNodeChanged) {
+            Resource modifcandidate = resource;
+            while (modifcandidate != null && !modifcandidate.isResourceType(TYPE_LAST_MODIFIED))
+                modifcandidate = modifcandidate.getParent();
+            if (modifcandidate != null) {
+                newvalues.put(PROP_LAST_MODIFIED, Calendar.getInstance());
+            }
+        }
     }
+
 }
