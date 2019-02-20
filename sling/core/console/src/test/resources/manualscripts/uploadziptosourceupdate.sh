685b7c5190a9dff8f5c692ff148887be6a0e4fa8
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-02-20 10:22:22 +0100

    somewhat working zip upload

--- sling/core/console/src/test/resources/manualscripts/uploadziptosourceupdate.sh
+++ sling/core/console/src/test/resources/manualscripts/uploadziptosourceupdate.sh
@@ -8,10 +8,10 @@ clear
 # curl -i -u admin:admin -v "http://localhost:9090/bin/cpm/nodes/debug/sourceupload.xml/content/test/composum/pages/intermediate/site/home/subsite"
 
 # create:
-# cd /Users/hps/dev/composum/private/sling/prototype/test/src/main/content/jcr_root
+# cd /Users/hps/dev/composum/private/sling/prototype/test/src/main/content
 # /bin/rm -f /Users/hps/Library/Preferences/IntelliJIdea2018.3/scratches/scratch.zip
-# zip -r -9 /Users/hps/Library/Preferences/IntelliJIdea2018.3/scratches/scratch.zip content/test/composum/pages/intermediate/site
+# zip -r -9 /Users/hps/Library/Preferences/IntelliJIdea2018.3/scratches/scratch.zip jcr_root/content/test/composum/pages/intermediate/site
 
 # Actual POST
-curl -u admin:admin -v -F "file=@/Users/hps/Library/Preferences/IntelliJIdea2018.3/scratches/scratch.zip" "http://localhost:9090/bin/cpm/nodes/debug/sourceupload.zip/content/test/composum/pages/intermediate/site/home/subsite"
+curl -u admin:admin -v -F "file=@/Users/hps/Library/Preferences/IntelliJIdea2018.3/scratches/scratch.zip" "http://localhost:9090/bin/cpm/nodes/debug/sourceupload.zip"
 
