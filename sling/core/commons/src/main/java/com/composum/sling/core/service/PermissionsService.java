dfad79ce985bc3d98d487906f4ecdbb1622ffef1
Ralf Wunsch <r.wunsch@ist-software.com>
2019-03-21 14:26:20 +0100

    include 'admin' always if group membership is checked

--- sling/core/commons/src/main/java/com/composum/sling/core/service/PermissionsService.java
+++ sling/core/commons/src/main/java/com/composum/sling/core/service/PermissionsService.java
@@ -8,16 +8,30 @@ import javax.jcr.Session;
 public interface PermissionsService {
 
     /**
-     * @return the first matching member or NULL if not a member
+     * @param session         the current session
+     * @param authorizableIds a list of group ids to check
+     * @return the first matching group found; NULL if no group found; probably "" if user is 'admin'
      */
     String isMemberOfOne(Session session, String... authorizableIds);
 
+    /**
+     * @param session         the current session
+     * @param authorizableIds a list of group ids to check
+     * @return 'true' if session user is member of all groups (or of THE group if only one is checked)
+     */
     boolean isMemberOfAll(Session session, String... authorizableIds);
 
     /**
-     * @return the first matching privilege or NULL if check fails
+     * @param session       the current session
+     * @param privilegeKeys a list of privilege keys to check
+     * @return the first privilege found; NULL if no privilege found
      */
     String hasOneOfPrivileges(Session session, String path, String... privilegeKeys);
 
+    /**
+     * @param session       the current session
+     * @param privilegeKeys a list of privilege keys to check
+     * @return 'true' if session user has all privileges (or has THE privilege if only one is checked)
+     */
     boolean hasAllPrivileges(Session session, String path, String... privilegeKeys);
 }
