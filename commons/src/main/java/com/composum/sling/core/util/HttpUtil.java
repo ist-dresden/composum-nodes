595b59de58be7e8fbe9a8d6564f9335b25a333ba
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-11-15 16:24:29 +0100

    fix HttpUtil.isModifiedSince to HttpUtil.notModifiedSince (in case of null lastModified the result was wrong)

--- commons/src/main/java/com/composum/sling/core/util/HttpUtil.java
+++ commons/src/main/java/com/composum/sling/core/util/HttpUtil.java
@@ -23,12 +23,55 @@ public class HttpUtil extends HttpConstants {
     public static final String HEADER_IF_NONE_MATCH = "If-None-Match";
 
     /**
-     * returns 'true' if 'lastModified' is after 'ifModifiedSince' (and both values are valid)
+     * Checks whether we can skip transmission of a resource because of a recent enough {@link #HEADER_IF_MODIFIED_SINCE}
+     * header.
+     * Returns true if the given lastModified date is after the {ifModifiedSince} or if there is no lastModified date,
+     * so that we don't know and have to transmit the resource, anyway.
+     *
+     * @param ifModifiedSince value of the {@link #HEADER_IF_MODIFIED_SINCE} header
+     * @param lastModified date of the resource to be submitted
+     * @return if the resource transmission can be skipped since the browser has the current version
      */
-    public static boolean isModifiedSince(long ifModifiedSince, Calendar lastModified) {
-        return lastModified != null && isModifiedSince(ifModifiedSince, lastModified.getTimeInMillis());
+    public static boolean notModifiedSince(long ifModifiedSince, Calendar lastModified) {
+        return lastModified != null && notModifiedSince(ifModifiedSince, lastModified.getTimeInMillis());
     }
 
+    /**
+     * Checks whether we can skip transmission of a resource because of a recent enough {@link #HEADER_IF_MODIFIED_SINCE}
+     * header.
+     * Returns true if the given lastModified date is after the {ifModifiedSince} or if there is no lastModified date,
+     * so that we don't know and have to transmit the resource, anyway.
+     *
+     * @param ifModifiedSince value of the {@link #HEADER_IF_MODIFIED_SINCE} header
+     * @param lastModified    date of the resource to be submitted
+     * @return if the resource transmission can be skipped since the browser has the current version
+     */
+    public static boolean notModifiedSince(long ifModifiedSince, Long lastModified) {
+        if (ifModifiedSince != -1 && lastModified != null) {
+            // ignore millis because 'ifModifiedSince' comes often without millis
+            long lastModifiedTime = lastModified / 1000L * 1000L;
+            ifModifiedSince = ifModifiedSince / 1000L * 1000L;
+            return lastModifiedTime <= ifModifiedSince;
+        }
+        return false;
+    }
+
+    /**
+     * @deprecated please use {@link #notModifiedSince(long, Calendar)} since that's cleaner; this will be removed
+     * soon
+     */
+    // FIXME(hps,15.11.19) remove this
+    @Deprecated
+    public static boolean isModifiedSince(long ifModifiedSince, Calendar lastModified) {
+        return !notModifiedSince(ifModifiedSince, lastModified);
+    }
+
+    /**
+     * @deprecated please use {@link #notModifiedSince(long, Long)} since that's cleaner; this will be removed
+     * soon
+     */
+    // FIXME(hps,15.11.19) remove this
+    @Deprecated
     public static boolean isModifiedSince(long ifModifiedSince, Long lastModified) {
         if (ifModifiedSince != -1 && lastModified != null) {
             // ignore millis because 'ifModifiedSince' comes often without millis
@@ -38,4 +81,6 @@ public class HttpUtil extends HttpConstants {
         }
         return true;
     }
+
+
 }
