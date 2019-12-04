b9305f865b8de2a51a54558dfc96d090a7954a2d
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2019-12-04 19:22:58 +0100

    SlingResourceUtil.appendPaths

--- sling/core/commons/src/test/java/com/composum/sling/core/util/SlingResourceUtilTest.java
+++ sling/core/commons/src/test/java/com/composum/sling/core/util/SlingResourceUtilTest.java
@@ -1,43 +1,65 @@
 package com.composum.sling.core.util;
 
-import org.hamcrest.Matchers;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
 
+import java.util.Arrays;
+
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
 
 public class SlingResourceUtilTest extends SlingResourceUtil {
 
     @Rule
-    public final ErrorCollector errorCollector = new ErrorCollector();
+    public final ErrorCollector ec = new ErrorCollector();
 
     @Test
     public void relativePath() {
-        errorCollector.checkThat(callRelativePath("bla", "bla/blu"), equalTo("blu"));
-        errorCollector.checkThat(callRelativePath("/", "/fol/der"), equalTo("fol/der"));
-        errorCollector.checkThat(callRelativePath("bla", "bla"), equalTo(""));
-        errorCollector.checkThat(callRelativePath("/bla", "/blu"), equalTo("../blu"));
-        errorCollector.checkThat(callRelativePath("/foo/bl/../la", "/foo/la/lu/../lu/lux"), equalTo("lu/lux"));
-        errorCollector.checkThat(callRelativePath("/bl/../la", "/la/lu/../lu/lux"), equalTo("lu/lux"));
-        errorCollector.checkThat(callRelativePath("/bl/la/lu", "/bl/la/x"), equalTo("../x"));
-        errorCollector.checkThat(callRelativePath("/bl/la/lu", "/bl/x/y"), equalTo("../../x/y"));
-        errorCollector.checkThat(callRelativePath("/blu/la/lu", "/bl/x/y"), equalTo("../../../bl/x/y"));
-        errorCollector.checkThat(callRelativePath("/blu/la/lu", "/"), equalTo("../../../"));
-        errorCollector.checkThat(callRelativePath("/blu/la/lu", "/x"), equalTo("../../../x"));
-        errorCollector.checkThat(callRelativePath("/", "/"), equalTo(""));
+        ec.checkThat(callRelativePath("bla", "bla/blu"), equalTo("blu"));
+        ec.checkThat(callRelativePath("/", "/fol/der"), equalTo("fol/der"));
+        ec.checkThat(callRelativePath("bla", "bla"), equalTo(""));
+        ec.checkThat(callRelativePath("/bla", "/blu"), equalTo("../blu"));
+        ec.checkThat(callRelativePath("/foo/bl/../la", "/foo/la/lu/../lu/lux"), equalTo("lu/lux"));
+        ec.checkThat(callRelativePath("/bl/../la", "/la/lu/../lu/lux"), equalTo("lu/lux"));
+        ec.checkThat(callRelativePath("/bl/la/lu", "/bl/la/x"), equalTo("../x"));
+        ec.checkThat(callRelativePath("/bl/la/lu", "/bl/x/y"), equalTo("../../x/y"));
+        ec.checkThat(callRelativePath("/blu/la/lu", "/bl/x/y"), equalTo("../../../bl/x/y"));
+        ec.checkThat(callRelativePath("/blu/la/lu", "/"), equalTo("../../../"));
+        ec.checkThat(callRelativePath("/blu/la/lu", "/x"), equalTo("../../../x"));
+        ec.checkThat(callRelativePath("/", "/"), equalTo(""));
     }
 
     private String callRelativePath(String parent, String child) {
         String relpath = relativePath(parent, child);
         // that's the invariant.
-        errorCollector.checkThat(ResourceUtil.normalize(parent + "/" + relpath), is(ResourceUtil.normalize(child)));
+        ec.checkThat(ResourceUtil.normalize(parent + "/" + relpath), is(ResourceUtil.normalize(child)));
         // check some variations
-        errorCollector.checkThat(relativePath(parent + "/", child), is(relpath));
-        errorCollector.checkThat(relativePath(parent + "/", child + "/"), is(relpath));
-        errorCollector.checkThat(relativePath(parent, child + "/"), is(relpath));
+        ec.checkThat(relativePath(parent + "/", child), is(relpath));
+        ec.checkThat(relativePath(parent + "/", child + "/"), is(relpath));
+        ec.checkThat(relativePath(parent, child + "/"), is(relpath));
         return relpath;
+    }
+
+    @Test
+    public void appendPaths() {
+        checkAppendPaths("/bla", "blu/bluf", "/bla/blu/bluf");
+        checkAppendPaths("/bla/bla/", "blu/bluf", "/bla/bla/blu/bluf");
+        checkAppendPaths("bla/la", "blu", "bla/la/blu");
+        checkAppendPaths("bla/la/", "/blu", "bla/la/blu");
+        checkAppendPaths("/", "blu", "/blu");
+        checkAppendPaths("/", "/blu", "/blu");
+        for (String childpath : Arrays.asList(null, "", "/")) {
+            for (String path : Arrays.asList("/", "/bla", "bla", "/bla/blu")) {
+                checkAppendPaths(path, childpath, path);
+            }
+        }
+    }
+
+    protected void checkAppendPaths(String path, String childpath, String result) {
+        ec.checkThat(path + " with " + childpath, appendPaths(path, childpath), equalTo(result));
+        ec.checkThat(path + " with " + childpath, StringUtils.startsWith(path, "/"),
+                equalTo(StringUtils.startsWith(result, "/")));
     }
 }
