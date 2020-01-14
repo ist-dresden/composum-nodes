c3a11e92264a4f83249858d7fcbf1b116457d307
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2020-01-14 14:29:46 +0100

    small i18n improvement

--- sling/core/commons/src/main/java/com/composum/sling/core/util/I18N.java
+++ sling/core/commons/src/main/java/com/composum/sling/core/util/I18N.java
@@ -1,10 +1,12 @@
 package com.composum.sling.core.util;
 
 import com.composum.sling.core.RequestBundle;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nonnull;
 import java.util.MissingResourceException;
 
 /**
@@ -14,10 +16,15 @@ public class I18N {
 
     private static final Logger LOG = LoggerFactory.getLogger(I18N.class);
 
-    public static String get(SlingHttpServletRequest request, String text) {
+    public static String get(@Nonnull SlingHttpServletRequest request, String text) {
         String translated = null;
         try {
             translated = RequestBundle.get(request).getString(text);
+            if (StringUtils.isBlank(translated)) {
+                LOG.warn("Suspicious translation to blank string ignored for '{}' locale {}", text,
+                        request.getLocale());
+                translated = null;
+            }
         } catch (MissingResourceException mrex) {
             if (LOG.isInfoEnabled()) {
                 LOG.info(mrex.toString());
