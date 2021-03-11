98bdf56ebc3946f6c8a95078f65e7ed7015aa005
Hans-Peter Stoerr <hp.stoerr@ist-software.com>
2021-03-11 09:07:32 +0100

    Source Update: use streaming archive; ignore ACL nodes when adjusting node order

--- console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
+++ console/src/main/java/com/composum/sling/nodes/update/SourceUpdateServiceImpl.java
@@ -3,13 +3,13 @@ package com.composum.sling.nodes.update;
 import com.composum.sling.core.ResourceHandle;
 import com.composum.sling.core.util.ResourceUtil;
 import org.apache.commons.collections.IteratorUtils;
-import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.vault.fs.api.ProgressTrackerListener;
+import org.apache.jackrabbit.vault.fs.config.DefaultWorkspaceFilter;
 import org.apache.jackrabbit.vault.fs.io.Importer;
-import org.apache.jackrabbit.vault.fs.io.MemoryArchive;
+import org.apache.jackrabbit.vault.fs.io.ZipStreamArchive;
 import org.apache.sling.api.resource.*;
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
@@ -24,6 +24,7 @@ import javax.jcr.nodetype.NodeDefinition;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.composum.sling.core.util.ResourceUtil.*;
 
@@ -39,7 +40,7 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
 
     /**
      * Attributes that are not modified on the target.
-     * See result of JCR query <code>/jcr:system/jcr:nodeTypes/*[jcr:isMixin=true]/rep:namedPropertyDefinitions//*[jcr:protected=true]</code>.
+     * See result of JCR query {@code /jcr:system/jcr:nodeTypes/*[jcr:isMixin=true]/rep:namedPropertyDefinitions//*[jcr:protected=true]}.
      */
     private static final Collection<String> ignoredMetadataAttributes = new HashSet<>(Arrays.asList("jcr:uuid", "jcr:lastModified",
             "jcr:lastModifiedBy", "jcr:created", "jcr:createdBy", "jcr:isCheckedOut", "jcr:baseVersion",
@@ -50,14 +51,14 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
 
     /**
      * Nodes that should not be removed from the target.
-     * See result of JCR query <code>/jcr:system/jcr:nodeTypes//element(*,nt:childNodeDefinition)[jcr:name]</code>
-     * and <code>/jcr:system/jcr:nodeTypes//rep:namedChildNodeDefinitions</code>
+     * See result of JCR query {@code /jcr:system/jcr:nodeTypes//element(*,nt:childNodeDefinition)[jcr:name]}
+     * and {@code /jcr:system/jcr:nodeTypes//rep:namedChildNodeDefinitions}
      */
     private static final Collection<String> noRemoveNodeNames = new HashSet<>(Arrays.asList("rep:policy", "oak:index", "rep:repoPolicy"));
 
     /**
      * Mixins that should not be removed from the target.
-     * See result of JCR query <code>/jcr:system/jcr:nodeTypes/*[jcr:isMixin=true]</code>
+     * See result of JCR query {@code /jcr:system/jcr:nodeTypes/*[jcr:isMixin=true]}
      */
     private static final Collection<String> noRemoveMixins = new HashSet<>(Arrays.asList(
             // various internal Jackrabbit stuff - we rather not touch that.
@@ -75,20 +76,20 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
     @Override
     public void updateFromZip(@Nonnull ResourceResolver resolver, @Nonnull InputStream rawZipInputStream, @Nonnull String nodePath)
             throws IOException, RepositoryException {
-        Session session = resolver.adaptTo(Session.class);
+        Session session = Objects.requireNonNull(resolver.adaptTo(Session.class));
         Resource tmpdir = makeTempdir(resolver);
         final String tmpPath = tmpdir.getPath();
 
         Importer importer;
         ImportErrorListener errorListener = new ImportErrorListener();
+        ZipStreamArchive archive = new ZipStreamArchive(rawZipInputStream);
         try {
             importer = new Importer();
-            // TODO(hps,11.02.20) Use ZipStreamArchive once that's public (in later vault versions)
-            MemoryArchive archive = new MemoryArchive(false);
-            archive.run(rawZipInputStream);
-            importer.getOptions().setStrict(true);
+            importer.getOptions().setFilter(new DefaultWorkspaceFilter());
             importer.getOptions().setListener(errorListener);
-            importer.run(archive, tmpdir.adaptTo(Node.class));
+            archive.open(true);
+            LOG.info("Importing {}", archive.getMetaInf().getProperties());
+            importer.run(archive, session, tmpdir.getPath());
         } catch (IOException | RepositoryException e) {
             throw e;
         } catch (Exception e) {
@@ -99,7 +100,7 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         if (importer.hasErrors()) {
             StringBuilder buf = new StringBuilder("Errors during import: ");
             errorListener.errors.forEach(e ->
-                    buf.append(e.getLeft()).append(" : ").append(String.valueOf(e.getRight())).append("\n"));
+                    buf.append(e.getLeft()).append(" : ").append(e.getRight()).append("\n"));
             throw new RepositoryException(buf.toString());
         }
 
@@ -143,27 +144,12 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         }
 
         try {
-            // first copy type information since this changes attributes
-            newvalues.put(PROP_PRIMARY_TYPE, templatevalues.get(PROP_PRIMARY_TYPE));
-            List<String> newMixins = new ArrayList<>(Arrays.asList(templatevalues.get(PROP_MIXINTYPES, new String[0])));
-            for (String mixin : newvalues.get(PROP_MIXINTYPES, new String[0])) {
-                if (noRemoveMixins.contains(mixin) && !newMixins.contains(mixin)) {
-                    newMixins.add(mixin);
-                }
-            }
-            if (!newMixins.isEmpty() || newvalues.containsKey(PROP_MIXINTYPES)) {
-                newvalues.put(PROP_MIXINTYPES, newMixins.toArray(new String[newMixins.size()]));
-            }
-
-            Node node = resource.adaptTo(Node.class);
-            NodeDefinition definition = node.getDefinition();
-            if (definition.allowsSameNameSiblings()) {
-                checkForSamenameSiblings(templateresource, resource);
-            }
+            copyTypeInformation(templatevalues, newvalues); // first this since it might change attributes.
+            checkForSamenameSiblings(templateresource, resource);
 
             for (Map.Entry<String, Object> entry : templatevalues.entrySet()) {
                 if (!ignoredMetadataAttributes.contains(entry.getKey()) &&
-                        ObjectUtils.notEqual(entry.getValue(), newvalues.get(entry.getKey()))) {
+                        !Objects.deepEquals(entry.getValue(), newvalues.get(entry.getKey()))) {
                     thisNodeChanged = true;
                     newvalues.put(entry.getKey(), entry.getValue());
                 }
@@ -203,9 +189,7 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
                 }
             }
 
-            if (node.getPrimaryNodeType().hasOrderableChildNodes()) {
-                ensureSameOrdering(templatechildren, resource);
-            }
+            ensureSameOrdering(templatechildren, resource);
 
             if (thisNodeChanged) {
                 Resource modifcandidate = resource;
@@ -222,42 +206,63 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
         }
     }
 
+    private void copyTypeInformation(ValueMap templatevalues, ModifiableValueMap newvalues) {
+        newvalues.put(PROP_PRIMARY_TYPE, templatevalues.get(PROP_PRIMARY_TYPE));
+        List<String> newMixins = new ArrayList<>(Arrays.asList(templatevalues.get(PROP_MIXINTYPES, new String[0])));
+        for (String mixin : newvalues.get(PROP_MIXINTYPES, new String[0])) {
+            if (noRemoveMixins.contains(mixin) && !newMixins.contains(mixin)) {
+                newMixins.add(mixin);
+            }
+        }
+        if (!newMixins.isEmpty() || newvalues.containsKey(PROP_MIXINTYPES)) {
+            newvalues.put(PROP_MIXINTYPES, newMixins.toArray(new String[0]));
+        }
+    }
+
     private void ensureSameOrdering(List<Resource> templatechildren, Resource resource) throws RepositoryException {
-        Node node = Objects.requireNonNull(resource.adaptTo(Node.class));
-        List<Resource> resourcechildren = IteratorUtils.toList(resource.listChildren());
-        if (templatechildren.size() != resourcechildren.size()) {
-            throw new IllegalStateException("Bug: template and resource of " + resource.getPath() +
-                    " should have same size now but have " + templatechildren.size() + " and " + resourcechildren.size());
-        }
-        if (resourcechildren.size() < 2) {
-            return;
-        }
-        Map<String, Resource> nameToNode = new HashMap<>();
-        for (Resource child : resourcechildren) {
-            nameToNode.put(child.getName(), child);
-        }
-        for (int i = 0; i < resourcechildren.size(); ++i) {
-            if (!StringUtils.equals(resourcechildren.get(i).getName(), templatechildren.get(i).getName())) {
-                node.orderBefore(templatechildren.get(i).getName(), resourcechildren.get(i).getName());
-                resourcechildren = IteratorUtils.toList(resource.listChildren());
+        if (resource.adaptTo(Node.class).getPrimaryNodeType().hasOrderableChildNodes()) {
+            templatechildren = filterNoRemoveNodes(templatechildren);
+            Node node = Objects.requireNonNull(resource.adaptTo(Node.class));
+            List<Resource> resourcechildren = filterNoRemoveNodes(IteratorUtils.toList(resource.listChildren()));
+            if (templatechildren.size() != resourcechildren.size()) {
+                throw new IllegalStateException("Bug: template and resource of " + resource.getPath() +
+                        " should have same size now but have " + templatechildren.size() + " and " + resourcechildren.size());
+            }
+            if (resourcechildren.size() < 2) {
+                return;
+            }
+            for (int i = 0; i < resourcechildren.size(); ++i) {
+                if (!StringUtils.equals(resourcechildren.get(i).getName(), templatechildren.get(i).getName())) {
+                    node.orderBefore(templatechildren.get(i).getName(), resourcechildren.get(i).getName());
+                    resourcechildren = filterNoRemoveNodes(IteratorUtils.toList(resource.listChildren()));
+                }
             }
         }
     }
 
-    private void checkForSamenameSiblings(Resource templateresource, @Nonnull Resource resource) throws IllegalArgumentException {
-        Set<String> nodenames = new HashSet<>();
-        for (Resource child : templateresource.getChildren()) {
-            if (nodenames.contains(child.getName())) {
-                throw new IllegalArgumentException("Equally named children not supported yet: existing resource " + templateresource.getPath() + " has two " + child.getName());
+    private List<Resource> filterNoRemoveNodes(List<Resource> children) {
+        return children.stream().filter(r -> !noRemoveNodeNames.contains(r.getName())).collect(Collectors.toList());
+    }
+
+    private void checkForSamenameSiblings(Resource templateresource, @Nonnull Resource resource)
+            throws IllegalArgumentException, RepositoryException {
+        Node node = resource.adaptTo(Node.class);
+        NodeDefinition definition = node.getDefinition();
+        if (definition.allowsSameNameSiblings()) {
+            Set<String> nodenames = new HashSet<>();
+            for (Resource child : templateresource.getChildren()) {
+                if (nodenames.contains(child.getName())) {
+                    throw new IllegalArgumentException("Equally named children not supported yet: existing resource " + templateresource.getPath() + " has two " + child.getName());
+                }
+                nodenames.add(child.getName());
             }
-            nodenames.add(child.getName());
-        }
-        nodenames.clear();
-        for (Resource child : resource.getChildren()) {
-            if (nodenames.contains(child.getName())) {
-                throw new IllegalArgumentException("Equally named children not supported yet: imported resource " + resource.getPath() + " has two " + child.getName());
+            nodenames.clear();
+            for (Resource child : resource.getChildren()) {
+                if (nodenames.contains(child.getName())) {
+                    throw new IllegalArgumentException("Equally named children not supported yet: imported resource " + resource.getPath() + " has two " + child.getName());
+                }
+                nodenames.add(child.getName());
             }
-            nodenames.add(child.getName());
         }
     }
 
@@ -267,12 +272,13 @@ public class SourceUpdateServiceImpl implements SourceUpdateService {
 
         @Override
         public void onMessage(Mode mode, String action, String path) {
-            // ignore
+            LOG.debug("Import message {} : {} : {}", mode, action, path);
         }
 
         @Override
         public void onError(Mode mode, String path, Exception e) {
             errors.add(Pair.of(path, e));
+            LOG.debug("Import error {} : {} : {}", mode, path, String.valueOf(e));
         }
     }
 }
