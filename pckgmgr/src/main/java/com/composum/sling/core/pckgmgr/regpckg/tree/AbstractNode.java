e83856d6be5538572020a657ae95865b06acfed4
Henry Kuijpers <henrykuijpers@apache.org>
2026-03-02 14:44:17 +0100

    #328 Remove reference to old Apache Commons Collections

--- pckgmgr/src/main/java/com/composum/sling/core/pckgmgr/regpckg/tree/AbstractNode.java
+++ pckgmgr/src/main/java/com/composum/sling/core/pckgmgr/regpckg/tree/AbstractNode.java
@@ -4,7 +4,7 @@ import com.composum.sling.core.pckgmgr.jcrpckg.tree.TreeItem;
 import com.composum.sling.core.util.JsonUtil;
 import com.google.gson.stream.JsonWriter;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
